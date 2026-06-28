8e845087d8e65242d3ef659cd2cea2416eafa73d
eve-o912 <everlynewangui30021@gmail.com>
2026-06-28 16:46:52 +0300

    Fix TanStackRouter import in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { TanStackRouterPlugin } from '@tanstack/router-plugin'
+import { TanStackRouter } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
   plugins: [
+    TanStackRouter(),
     react(),
-    TanStackRouterPlugin(),
   ],
 })
